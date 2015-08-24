c8ee67050be4fec30aa077def074e3328b9d85be
Ben Chatelain <ben@octop.ad>
2015-08-24 15:47:32 -0600

    Add Mac target

---
+++ ssh2-Mac/ssh2-Mac.h
@@ -0,0 +1,19 @@
+//
+//  ssh2-Mac.h
+//  ssh2-Mac
+//
+//  Created by Ben Chatelain on 8/24/15.
+//  Copyright © 2015 phatblat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ssh2-Mac.
+FOUNDATION_EXPORT double ssh2_MacVersionNumber;
+
+//! Project version string for ssh2-Mac.
+FOUNDATION_EXPORT const unsigned char ssh2_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssh2_Mac/PublicHeader.h>
+
+
