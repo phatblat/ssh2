16df1464b8c349c879b41b44d30a798295cfbb9c
Ben Chatelain <ben@octop.ad>
2015-08-24 15:41:47 -0600

    Create ssh2 project

---
+++ ssh2-iOS/ssh2-iOS/ssh2-iOS.h
@@ -0,0 +1,19 @@
+//
+//  ssh2-iOS.h
+//  ssh2-iOS
+//
+//  Created by Ben Chatelain on 8/24/15.
+//  Copyright © 2015 phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ssh2-iOS.
+FOUNDATION_EXPORT double ssh2_iOSVersionNumber;
+
+//! Project version string for ssh2-iOS.
+FOUNDATION_EXPORT const unsigned char ssh2_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssh2_iOS/PublicHeader.h>
+
+
